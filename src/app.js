7f3ec08ae6d49f04a94eb302c938e3331e768e4a
HeitorDenech <heitor.denech@gmail.com>
2023-11-03 18:05:06 -0300

    apiDOcumentada

--- src/app.js
+++ src/app.js
@@ -7,11 +7,17 @@ const app = express();
 // Importar as rotas para serem executadas na aplicação
 const crudRouter = require('./routes/crudRouter');
 
+// Importar as rotas para serem executadas na aplicação
+const alunosRouter = require('./routes/alunosRouter');
+
 // Importar o pacote dotenv
 const dotenv = require('dotenv').config();
 
 // Habilitar a utilização do crudRouter
 app.use('/api', crudRouter);
+
+// Habilitar a utilização do crudRouter
+app.use('/api', alunosRouter);
 
 // Setar a porta do servidor, a partir do arquivo .env
 app.set('port', process.env.PORT);
